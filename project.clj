091f1fa3cae96f2f2569847951345411db0e13b0
Chris Allen <cma@bitemyapp.com>
2013-08-16 03:27:36 -0700

    setting version and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject bulwark "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject bulwark "0.0.1"
+  :description "Defensive Ring middleware like Rack::attack."
+  :url "http://github.com/bitemyapp/bulwark"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
