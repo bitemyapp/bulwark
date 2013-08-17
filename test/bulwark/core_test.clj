6bf09377df06d65835733cff04ebdc51089f34c7
Chris Allen <cma@bitemyapp.com>
2013-08-16 23:58:27 -0700

    testability sucks with atom.

--- test/bulwark/core_test.clj
+++ test/bulwark/core_test.clj
@@ -5,9 +5,14 @@
             [ring.mock.request :refer :all]))
 
 (deftest test-exercise-with-mock
-  (testing "FIXME, I fail."
+  (testing "Test blacklisting"
     (let [protect (protect-middleware {})
           _ (blacklist "nothing touches /untouchable" (fn [req] (not= (:uri req) "/untouchable")))
           _ (whitelist "only /okay is allowed"        (fn [req] (= (:uri req) "/okay")))
-          _ (throttle  "localhost is throttled" 3 10  (fn [req] ((:remote-addr req))))]
-      (is (= 1 1)))))
+          _ (throttle  "localhost is throttled" 3 10  (fn [req] (:remote-addr req)))]
+      (is (= (:status (protect (request :get "/untouchable")))
+             403))))
+  (testing "Test whitelisting"
+    (is (= 0 0)))
+  (testing "Test throttling"
+    (is (= 0 0))))
